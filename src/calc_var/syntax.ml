756fc7d0a9a274dd178e4648331ac9a7c0bc4ec8
Vasile Rotaru <vrotaru.md@gmail.com>
2019-09-12 23:23:53 +0300

    Pretty-print precedence for Negate fixed
    Without the fix the following is true
    
    ```OCaml
    string_of_expression @@ Negate (Plus (Number 1, Number 2)) = "-1 + 2"
    ```

--- src/calc_var/syntax.ml
+++ src/calc_var/syntax.ml
@@ -21,7 +21,7 @@ let string_of_expression e =
     let (m, str) = match e with
       | Variable x      ->    (3, x)
       | Numeral n       ->    (3, string_of_int n)
-      | Negate e        ->    (2, "-" ^ (to_str 0 e))
+      | Negate e        ->    (2, "-" ^ (to_str 2 e))
       | Times (e1, e2)  ->    (1, (to_str 1 e1) ^ " * " ^ (to_str 2 e2))
       | Divide (e1, e2) ->    (1, (to_str 1 e1) ^ " / " ^ (to_str 2 e2))
       | Plus (e1, e2)   ->    (0, (to_str 0 e1) ^ " + " ^ (to_str 1 e2))
