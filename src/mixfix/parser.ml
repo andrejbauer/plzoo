bf26872cdddaa8d1b4bc1c7e6d8b4c17c39414f4
Aljaž Medič <35614267+aljazmedic@users.noreply.github.com>
2024-02-05 10:15:40 +0100

    Monadic approach v1

--- src/mixfix/parser.ml
+++ src/mixfix/parser.ml
@@ -3,84 +3,64 @@
 (** Explicit Maybe *)
 type name_part = Presyntax.expr
 
-type 'a parse_result = Error | Result of ('a * name_part list) list
+type 'a parse_result = ('a * name_part list)
 
-let proj = function
-  | Error -> []
-  | Result s -> s
+type 'a parser = name_part list -> 'a parse_result list
 
+(*
+Notes:
+Sm probal Error | Result [] -> Vseeno če samo [], kjer prazen seznam pomeni error.
+Sm probal s flatmap = List.flatten List.map ...  -> obstaja List.concat_map, manj teksta :)
+Sm probal samo list monad-ish, ampak potem 'spodaj' ostane še ('a parse_result), in je vseeno če oboje v eno monado damo.
+*)
 
-type 'a parser = name_part list -> 'a parse_result
-
-let fail :'a parser = fun _ -> Error
-
-let (||) (p1:'a parser) (p2:'a parser):'a parser = 
-  fun inp ->  let r1 = proj (p1 inp) in let r2 = proj (p2 inp) in
-  Result (r1 @ r2)
+let fail :'a parser = fun _ -> []
 ;;
 
-let star (fun_parser: ('a -> 'b) parser) (x_parser: 'a parser):'b parser = 
-  fun inp ->
-    match fun_parser inp with
-    | Error -> Error
-    | Result h_list -> 
-      let rec loop = function
-        | [] -> []
-        | (f, rest)::t -> 
-          let x_parse = x_parser rest in
-          match x_parse with
-          | Error -> loop t
-          | Result h_list -> 
-            List.map (fun (x, rest) -> (f x, rest)) h_list @ (loop t)
-    in Result (loop h_list)
+let return (x:'a) : 'a parser = fun s -> [(x, s)]
+;;
+
+let (>>=) (x: 'a parser) (f:'a -> 'b parser): 'b parser= 
+  fun s ->
+    let g (x, rest) = (f x) rest in
+    List.concat_map g (x s)
+;;
+
+let (||) (p1:'a parser) (p2:'a parser):'a parser = 
+  fun inp ->  (p1 inp) @ (p2 inp)
+;;
+
+(** Application ? *)
+(** Applicatove functor operator** <- več o tem *)
+
+
+let star (fun_parser: ('a -> 'b) parser) (x_parser: 'a parser): 'b parser = 
+  fun_parser >>= (fun f -> x_parser >>= (fun x -> return (f x)))
 ;;
 
 let dolla (f: 'a->'b) (x_parser: 'a parser): 'b parser = 
-  fun inp -> 
-    match x_parser inp with
-    | Error -> Error
-    | Result h_list -> Result (List.map (fun (x, rest) -> (f x, rest)) h_list)
+  x_parser >>= (fun x -> return (f x))
 ;;
-
-let flatmap f l = List.flatten (List.map f l)
-;;
-
-(** Applies function on all outcomes. Useful for writing a function for one output and just applying it to all outcomes *)
-
-let flatmap_result (f:('a * name_part list) -> 'a parse_result) (l:('a * name_part list) list) = 
-  flatmap proj (List.map f l)
-;;
-
-(* 
-
-type 'a nonempty =
-  | First of 'a
-  | (::) of ('a * 'a nonempty)
-;;
-
-*)
-
 
 let iter (p:'a parser): 'a list parser = 
-  fun s ->
-  let rec f = fun s ->
-  match p s with
-    | Error -> [([], s)]
-    | Result options ->
-      let map (head, leftover) = (List.map (fun (tail, leftover) -> (head::tail, leftover)) (f leftover)) 
-    in flatmap map options
-  in 
-  Result(f s)
+  let rec iter' p =
+    p >>= (fun x -> iter' p >>= (fun xs -> return (x::xs)))
+  in
+  iter' p
+;;
 
-let iterplus (p: 'a parser): 'a list parser =
-  fun s ->
-    match (iter p s) with
-      | Error | Result [] -> Error
-      | Result _ as result -> result
-    ;;
-
-let between (p: 'a parser) (tokens: string list): 'a list parser =
-  fun s -> 
-    match p s with
-      | Error -> Error
-      | Result
+(* Tuki nekaj ne štima, mogoče pa vseeno rabimo explicit Error ? *)
+(* Zna biti da je option čisto odveč. Ne smemo kar vrnit empty seznama, če so še tokeni !! *)
+let between (p: 'a parser) (tokens: name_part list) =
+  let rec between' p tokens = 
+    fun s -> 
+    match (tokens, s) with
+      | [], _ -> return ([], true) (* Edge case: We ran out of tokens, everything is ok *)
+      | (t::ts, s1::s2) when t = s1 -> 
+        p >>= (fun x -> 
+          (between' p ts s2 >>= (fun (xs, allok) -> 
+            (if allok then return ((x::xs), allok) else return ([], allok)))))
+      | _ -> return ([], false) (* Error case, one of the tokens didn't match *)
+  in
+  fun s -> (between' p tokens s) >>= (fun (x, _) -> return x)
+  ;;
